bcb0172d72608f19bc33ed0b655ab9de650101c0
Denny Hong <hong961127@gmail.com>
2022-01-03 22:36:22 -0800

    Render my first object on a scene

--- start/lecture2_1/app.js
+++ start/lecture2_1/app.js
@@ -1,21 +1,16 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from "../../libs/three/three.module.js";
+import { OrbitControls } from "../../libs/three/jsm/OrbitControls.js";
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
-    }
-    
-	render( ) {   
-        
-    }
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+    window.addEventListener("resize", this.resize.bind(this));
+  }
+
+  resize() {}
+
+  render() {}
 }
 
 export { App };
--- start/lecture2_2/app.js
+++ start/lecture2_2/app.js
@@ -1,22 +1,68 @@
-import * as THREE from '../../libs/three/three.module.js';
-import { OrbitControls } from '../../libs/three/jsm/OrbitControls.js';
+import * as THREE from "../../libs/three/three.module.js";
+import { OrbitControls } from "../../libs/three/jsm/OrbitControls.js";
 
-class App{
-	constructor(){
-		const container = document.createElement( 'div' );
-		document.body.appendChild( container );
-    
-        
-        window.addEventListener('resize', this.resize.bind(this) );
-	}	
-    
-    resize(){
-        
-    }
-    
-	render( ) {  
-        
-    }
+class App {
+  constructor() {
+    const container = document.createElement("div");
+    document.body.appendChild(container);
+
+    // We need a scene so we can add objects and lights
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0xaaaaaa);
+
+    // Add lighting to scene
+    // ambient light does not illuminate based on location or position
+    // HemisphereLight has different color for surfaces pointing down and up
+    const ambient = new THREE.HemisphereLight(0xffffff, 0xbbbbff, 0.3);
+    this.scene.add(ambient);
+    // DirectionalLight points from its position to the origin or a target object
+    const light = new THREE.DirectionalLight();
+    light.position.set(0.2, 1, 1);
+    this.scene.add(light);
+
+    // Camera acts as the viewer's position and orientation in the scene
+    // PerspectiveCamera means closer objects appear larger, furthur objects appear smaller
+    this.camera = new THREE.PerspectiveCamera(
+      60, // Field of View (DEG)
+      window.innerWidth / window.innerHeight, // Aspect ratio of rendered view
+      0.1, // Near value, anything closer than this will be hidden
+      100 // Far value, anything furthur than this will be hidden
+    );
+    this.camera.position.set(0, 0, 4); // x (east) ➡️, y (up) ⬆️, z (south) ↙️
+
+    // { antialias: true } is important for VR apps, otherwise apps will suffer from jagged edges
+    this.renderer = new THREE.WebGLRenderer({ antialias: true });
+    this.renderer.setPixelRatio(window.devicePixelRatio); // Prevent blurry graphics
+    this.renderer.setSize(window.innerWidth, window.innerHeight); // Full window
+    this.renderer.setAnimationLoop(this.render.bind(this)); // setAnimationLoop is called up to 60 times/sec
+    // When a render is created, it creates a <canvas/> DOM element
+    container.appendChild(this.renderer.domElement);
+
+    // Add an object
+    const geometry = new THREE.BoxBufferGeometry(); // 1 unit wide, high, deep
+    const material = new THREE.MeshStandardMaterial({ color: 0xff0000 });
+    this.mesh = new THREE.Mesh(geometry, material); // an object is usually a mesh instance
+    this.scene.add(this.mesh);
+
+    // Allow user to interact with the camera
+    const controls = new OrbitControls(this.camera, this.renderer.domElement);
+
+    window.addEventListener("resize", this.resize.bind(this), {
+      passive: true,
+    });
+  }
+
+  resize() {
+    this.camera.aspect = window.innerWidth / window.innerHeight;
+    this.camera.updateProjectionMatrix();
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+  }
+
+  render() {
+    console.log("render called");
+    this.mesh.rotateY(0.01);
+    this.renderer.render(this.scene, this.camera);
+  }
 }
 
 export { App };
